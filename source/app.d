92d76f691b1502e2416fb2c61045c1ee3d636d25
harshith1.sheshan@gmail.com <harshith1.sheshan@gmail.com>
2023-04-10 01:59:21 -0400

    MCV with server client with basic chat broadcast

--- source/app.d
+++ source/app.d
@@ -1,10 +1,34 @@
 import std.stdio;
-
 import controller;
+public import server;
+public import client;
 
 
-void main(){
-  SDLApp myApp = new SDLApp();
-	myApp.MainApplicationLoop();
-	
+
+
+void main(string[] args){
+
+	//TCPServer server = new TCPServer;
+	//server.run();
+
+	//TCPClient client = new TCPClient();
+	//client.run();
+	//writeln("args:"~args[]~"and"~args[1]);
+	if(args.length != 2){
+		writeln("Invalid arguments");
+	}
+	else{
+		if(args[1]=="server"){
+			TCPServer server = new TCPServer();
+			server.run();
+		}
+		else if( args[1] == "client"){
+			TCPClient client = new TCPClient();
+			client.run();
+		}
+	  SDLApp myApp = new SDLApp();
+		myApp.MainApplicationLoop();
+	}
+	//SDLApp myApp = new SDLApp();
+	//myApp.MainApplicationLoop();
 }
