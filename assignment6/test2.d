6d874d7ae88a3a7371ffc27a057851f2be7ff97f
Harshith Sheshan <harshith1.sheshan@gmail.com>
2023-04-17 00:33:52 -0400

    Delete reference directory

--- assignment6/test2.d
+++
@@ -1,8 +0,0 @@
-
-import mysdlapp;
-@("Test initialization of SDLApp and Surface test2.d")
-unittest{
-    SDLApp app = new SDLApp();
-    assert(app.s.imgSurface != null);
-    assert(app.s.window != null);
-}
