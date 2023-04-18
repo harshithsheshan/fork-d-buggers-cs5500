ec0f758312db60276267c9b262cd155acc47085b
harshparmar <parmarharsh4296@gmail.com>
2023-04-17 21:41:15 -0400

    Tests added for save and open

--- source/tests.d
+++ source/tests.d
@@ -1,15 +1,22 @@
-//module tests;
-//
-//import bindbc.sdl;
-//import controller:SDLApp;
-//import surface:Surface;
-//import loader = bindbc.loader.sharedlib;
-//
-//@("check if file saved successfully")
-//unittest{
-//    auto app = new SDLApp();
-//    auto imgSurface = new Surface();
-//    imgSurface.UpdateSurfacePixel(100, 200);
-//    bool saveResult = imgSurface.save("testFile");
-//    assert(saveResult);
-//}
+module tests;
+
+import bindbc.sdl;
+import controller:Client;
+import server:Server;
+import model:Surface;
+import loader = bindbc.loader.sharedlib;
+import std.stdio;
+
+private SDLSupport ret;
+
+@("check if file saved and opened successfully")
+unittest{
+    ret = loadSDL();
+    Surface s = new Surface();
+    s.UpdateSurfacePixel(100, 200);
+    bool saveResult = s.save("testFile");
+    assert(saveResult);
+
+    bool openResult = s.open("testFile");
+    assert(openResult);
+}
