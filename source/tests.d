71b836c1ca4c6f1d9500ce896ec9751ebea5190b
harshparmar <parmarharsh4296@gmail.com>
2023-04-18 00:30:41 -0400

    Client server communication testing in progress

--- source/tests.d
+++ source/tests.d
@@ -52,6 +52,36 @@ unittest{
     //assert(true);
 }
 
+//@("test networking")
+//unittest{
+//    //ret = loadSDL();
+//    string host = "localhost";
+//    ushort port = 50001;
+//    writeln("server connected");
+//    Server server = new Server();
+//    writeln("Server created");
+//    Socket clientS = new Socket(AddressFamily.INET, SocketType.STREAM);
+//    clientS.connect(new InternetAddress(host, port));
+//    Thread newthread = new Thread({
+//        while (true) {
+//            auto newClientSocket = server.mListeningSocket.accept();
+//            newClientSocket.send("Hello friend\0");
+//            writeln("message sent");
+//        }
+//    });
+//    newthread.start();
+//    writeln("Thread started");
+//    //port = 50001;
+
+//    char[80] buffer;
+//    auto received = clientS.receive(buffer);
+//    writeln(buffer[0 .. received]);
+//
+//    clientS.close();
+//    //server.mListeningSocket.close();
+//    newthread.join();
+//    assert(true);
+//}
 
 
 
